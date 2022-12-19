50fd300b1cf34a1be222018fade2c331a62e3826
pacodelarco <fdelarcodaw@gmail.com>
2022-12-19 21:32:15 +0100

    Examen finalizado

--- ExamenRecuperacion1_FranciscodelArco_DEINT/src/main/java/PantallaPrincipal.java
+++ ExamenRecuperacion1_FranciscodelArco_DEINT/src/main/java/PantallaPrincipal.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 public class PantallaPrincipal extends JPanel {
@@ -12,7 +13,7 @@ public class PantallaPrincipal extends JPanel {
     String codigo; // debe ser alfanumerico y max 10 car
     String modelo; // debe ser alfanumerico y max 15 car
     String marca; // max 10 car
-    String[] tipoMotor = {"gasolina", "diesel", "hibrido", "electrico"};
+    String[] tipoMotor = {"Gasolina", "Diesel", "Hibrido", "Electrico"};
     String[] tipoVehiculo = {"Turismo", "Monovolumen", "SUV", "Todoterreno"};
     String[] tipoCambio = {"Manual", "Automatico"};
     DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -87,6 +88,106 @@ public class PantallaPrincipal extends JPanel {
         add(marcaTextArea);
         marca = marcaTextArea.getText();
 
+
+        JRadioButton gasolinaButton = new JRadioButton(tipoMotor[0]);
+        gasolinaButton.setBounds(100, 250, 250, 30);
+        gasolinaButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(gasolinaButton);
+
+        JRadioButton dieselButton = new JRadioButton(tipoMotor[1]);
+        dieselButton.setBounds(350, 250, 250, 30);
+        dieselButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(dieselButton);
+
+        JRadioButton hibridoButton = new JRadioButton(tipoMotor[2]);
+        hibridoButton.setBounds(600, 250, 250, 30);
+        hibridoButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(hibridoButton);
+
+        JRadioButton electricoButton = new JRadioButton(tipoMotor[3]);
+        electricoButton.setBounds(850, 250, 250, 30);
+        electricoButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(electricoButton);
+
+
+        //Tipo de vehiculo
+        JRadioButton turismoButton = new JRadioButton(tipoVehiculo[0]);
+        turismoButton.setBounds(100, 320, 250, 30);
+        turismoButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(turismoButton);
+
+        JRadioButton monovolumenButton = new JRadioButton(tipoVehiculo[1]);
+        monovolumenButton.setBounds(350, 320, 250, 30);
+        monovolumenButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(monovolumenButton);
+
+        JRadioButton SUVButton = new JRadioButton(tipoVehiculo[2]);
+        SUVButton.setBounds(600, 320, 250, 30);
+        SUVButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(SUVButton);
+
+        JRadioButton todoterrenoButton = new JRadioButton(tipoVehiculo[3]);
+        todoterrenoButton.setBounds(850, 320, 250, 30);
+        todoterrenoButton.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(todoterrenoButton);
+
+
+        //tipo de caja de cambios
+        JComboBox<String> tipoCajaCambios = new JComboBox<>(tipoCambio);
+        tipoCajaCambios.setBounds(100, 400, 250, 30);
+        tipoCajaCambios.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        tipoCajaCambios.setEnabled(false);
+        if((turismoButton.isSelected()&&gasolinaButton.isSelected()) || (monovolumenButton.isSelected()&&dieselButton.isSelected())){
+            tipoCajaCambios.setEnabled(true);
+        };
+        add(tipoCajaCambios);
+
+
+        // Spinner de la fecha de alta
+        Date hoy = new Date();
+        JSpinner fechaAltaSpinner = new JSpinner(new SpinnerDateModel(hoy, null, null, Calendar.MONTH));
+        JSpinner.DateEditor editor = new JSpinner.DateEditor(fechaAltaSpinner, "dd/MM/yyyy");
+        fechaAltaSpinner.setEditor(editor);
+        fechaAltaSpinner.setBounds(470, 400, 250, 30);
+        fechaAltaSpinner.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
+        add(fechaAltaSpinner);
+
+
+        //proveedor
+        JTextArea proveedorTextArea = new JTextArea("Proveedor");
+        proveedorTextArea.setBounds(820, 400, 250, 30);
+        proveedorTextArea.setFont(new Font("Microsoft PhagsPa", Font.ITALIC, 18));
+        proveedorTextArea.setToolTipText("Introduzca proveedor de vehículo");
+
+
+        proveedorTextArea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                proveedorTextArea.setText("");
+                proveedorTextArea.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 18));
+            }
+        });
+        add(proveedorTextArea);
+        proveedor = proveedorTextArea.getText();
+
+
+        //telefono de contacto
+        JTextArea contactoTextArea = new JTextArea("Telf. Contacto");
+        contactoTextArea.setBounds(100, 500, 250, 30);
+        contactoTextArea.setFont(new Font("Microsoft PhagsPa", Font.ITALIC, 18));
+
+        contactoTextArea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                contactoTextArea.setText("");
+                contactoTextArea.setFont(new Font("Microsoft PhagsPa", Font.PLAIN, 18));
+
+            }
+        });
+        add(contactoTextArea);
+        codigo = contactoTextArea.getText();
+
+
         //Botones
         JButton aceptar = new JButton("Aceptar");
         aceptar.setBounds(300, 700, 250, 30);
@@ -98,7 +199,6 @@ public class PantallaPrincipal extends JPanel {
         cancelar.setBounds(650, 700, 250, 30);
         cancelar.setFont(new Font("Microsoft PhagsPa", Font.BOLD, 18));
         add(cancelar);
-
 
     }
 
